aa1ae22f86fa30d37c28f62aa20fe31a8ee36c0c
SavenkovN <nek.sav@mail.ru>
2024-10-19 00:09:51 +0400

    Финал: Часть 2

---
+++ src/test/java/hooks/ApiHooks.java
@@ -0,0 +1,15 @@
+package hooks;
+
+import io.qameta.allure.Attachment;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ApiHooks {
+
+    @Attachment(value = "Читаемый Json файл", type = "application/json", fileExtension = ".txt")
+    public static byte[] getJson(String resourceName) throws IOException {
+        return Files.readAllBytes(Paths.get("src/test/resources", resourceName));
+    }
+
+}
